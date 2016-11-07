4340a5eef4a600d3a2613a3e6218ef328d073e8f
Benjamin Tidman <btidman@pillartechnology.com>
2016-11-07 15:48:38 -0500

    created index.html added basic karma test

---
+++ src/app.js
@@ -0,0 +1,14 @@
+
+
+
+function InputReader(){
+    this.readInput = function(){
+            return $("#name").val();
+        }
+}
+
+// Export node module.
+if ( typeof module !== 'undefined' && module.hasOwnProperty('exports') )
+{
+    module.exports = InputReader;
+}
